fa6c179076130bf20fb23cc6240c8778c28438e6
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2026-03-22 14:10:20 +0530

    added the project

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -3,12 +3,17 @@ import eccomerce from "../../public/eccomerce.png"
 import eccomerce_pdp from "../../public/eccomerce_pdp.png"
 import Github from "./Github"
 import { motion } from "framer-motion"
+import chat from "../../public/chat-app-2.png"
+import chat1 from "../../public/chatapp-1.png"
 
 export const Projects = () => {
 
+ 
     const [isHover,setIsHover]=useState(false)
 
     const skills=["React Js","Express Js","Postgress Sql","Tailwind CSS","JavaScript","Stripe Api","Cloudinary Api","JWT"]
+
+     const skills_chat=["React Js","Express Js","Mongodb","Tailwind CSS","JavaScript","Gemini Api","Cloudinary Api","JWT","Socket io"]
     return (
          <section className="mt-16 sm:mt-20" id="projects">
             <motion.h1 
@@ -43,6 +48,41 @@ export const Projects = () => {
                     <div className="flex items-center justify-between mt-6">
                       <button className="px-3 py-2 text-sm text-black bg-green-100 hover:bg-green-200 rounded-xl cursor-pointer hover:underline">Demo Link</button>
                       <a href="https://github.com/inder295/Ecommerce.git">
+                        <button><Github/></button>
+                      </a>
+                    </div> 
+
+                </motion.div>     
+
+                
+            </motion.div> 
+
+             <motion.div className="mt-8 sm:mt-10"
+             >
+               <motion.div className="w-full max-w-3xl mx-auto mt-8 shadow-md shadow-gray-200 rounded-xl p-4 sm:p-6 transition-transform duration-300 hover:shadow-xl hover:bg-gray-50"
+               initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+                viewport={{ once: true, amount: 0.3 }}>
+                <img src={isHover ? chat1 : chat} alt="chat-app"
+                    onMouseEnter={()=>{setIsHover(true)}}
+                    onMouseLeave={()=>{setIsHover(false)}}
+                    className="w-full rounded-lg"
+                />
+                   <h2 className="text-lg sm:text-xl font-semibold mt-4">Real Time Chat App </h2>
+                   <p className="text-sm sm:text-base mt-2 text-gray-700">A full-stack real-time chat application built using the MERN stack with Socket.io for instant messaging. The app supports private chats, file sharing, message replies, and real-time online user tracking with a scalable backend architecture.</p>
+                   <div className="flex flex-wrap gap-2 mt-3">
+                    {
+                       skills_chat.map((skill,index)=>{
+                        return <span key={index} className="px-3 py-1.5 bg-gray-200 rounded-2xl text-xs sm:text-sm">{skill}</span>
+                       })
+                    }
+                   </div>
+                    <div className="flex items-center justify-between mt-6">
+                        <a href="https://chat-app-wsb1.onrender.com/">
+                      <button className="px-3 py-2 text-sm text-black bg-green-100 hover:bg-green-200 rounded-xl cursor-pointer hover:underline">Demo Link</button>
+                      </a>
+                      <a href="https://github.com/inder295/Chat-App-">
                         <button><Github/></button>
                       </a>
                     </div> 
